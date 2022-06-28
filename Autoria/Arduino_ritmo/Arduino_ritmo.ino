c0ed4f5fbad5589067a42fcc9f626db50f767c63
Yasmin Vieira da Silva <102532302+yasminvieirasilva@users.noreply.github.com>
2022-06-28 15:00:34 -0300

    Update Arduino_ritmo.ino

--- Autoria/Arduino_ritmo/Arduino_ritmo.ino
+++ Autoria/Arduino_ritmo/Arduino_ritmo.ino
@@ -6,38 +6,38 @@ void setup()
 void loop()
 {
   digitalWrite(4, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(4, LOW);
-  delay(100); // Wait for 1000 millisecond(s)
+  delay(100); 
    digitalWrite(4, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(4, LOW);
   digitalWrite(4, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(4, LOW);
-  delay(100); // Wait for 1000 millisecond(s) digitalWrite(4, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(100); 
+  delay(1000); 
   digitalWrite(4, LOW);
-  delay(100); // Wait for 1000 millisecond(s) 
+  delay(100); 
    digitalWrite(5, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(5, LOW);
-  delay(100); // Wait for 1000 millisecond(s)  
+  delay(100); 
      digitalWrite(2, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000);
   digitalWrite(2, LOW);
-  delay(100); // Wait for 1000 millisecond(s) 
+  delay(100); 
    digitalWrite(5, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(5, LOW);
-  delay(100); // Wait for 1000 millisecond(s)
+  delay(100); 
    digitalWrite(5, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(5, LOW);
-  delay(100); // Wait for 1000 millisecond(s)
+  delay(100); 
   digitalWrite(2, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(2, LOW);
-  delay(100); // Wait for 1000 millisecond(s)
+  delay(100); 
  
 }
