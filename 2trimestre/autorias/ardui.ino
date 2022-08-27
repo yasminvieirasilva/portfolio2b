f57fa56a3ce25b2e7533088eb7a54f643f49059e
Yasmin Vieira da Silva <102532302+yasminvieirasilva@users.noreply.github.com>
2022-08-27 17:41:58 -0300

    Update and rename ardui to ardui.ino

---
+++ 2trimestre/autorias/ardui.ino
@@ -0,0 +1,43 @@
+void setup()
+{
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  delay(1000); 
+  digitalWrite(4, LOW);
+  delay(100); 
+   digitalWrite(4, HIGH);
+  delay(1000); 
+  digitalWrite(4, LOW);
+  digitalWrite(4, HIGH);
+  delay(1000); 
+  digitalWrite(4, LOW);
+  delay(100); 
+  delay(1000); 
+  digitalWrite(4, LOW);
+  delay(100); 
+   digitalWrite(5, HIGH);
+  delay(1000); 
+  digitalWrite(5, LOW);
+  delay(100); 
+     digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(100); 
+   digitalWrite(5, HIGH);
+  delay(1000); 
+  digitalWrite(5, LOW);
+  delay(100); 
+   digitalWrite(5, HIGH);
+  delay(1000); 
+  digitalWrite(5, LOW);
+  delay(100); 
+  digitalWrite(2, HIGH);
+  delay(1000); 
+  digitalWrite(2, LOW);
+  delay(100); 
+ 
+}
